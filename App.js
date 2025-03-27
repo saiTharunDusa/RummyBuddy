004c50cfdbb482993a8674d5eb2e879c10ee0cd7
Sai Tharun Dusa <sd2672@nau.edu>
2025-03-27 00:26:15 -0700

    Initial Project Setup

---
+++ App.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+import {
+  ScrollView,
+  SafeAreaView, Text
+} from 'react-native';
+
+
+const App = () => {
+  
+
+  return (
+    <SafeAreaView>
+      <Text>
+        Rummy ScoreBoard!
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * @format
  */
+import './gesture-handler.js';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
